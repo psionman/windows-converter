582447a28e2a2becf2eb12d09fb40c4746905b67
Jeff <jeffwatkins2000@gmail.com>
2025-09-04 11:51:58 +0100

    UPdate pyinstaller and pyproject based on Windows test

--- src/data/pyinstaller.py
+++ src/data/pyinstaller.py
@@ -2,7 +2,7 @@ import PyInstaller.__main__
 from pathlib import Path
 
 HERE = Path(__file__).parent.absolute()
-path_to_main = str(Path(HERE, '<project_dir>', 'main.py'))
+path_to_main = str(Path(HERE, 'main.py'))
 
 
 def install():
@@ -11,6 +11,6 @@ def install():
         '--onefile',
         '--windowed',
         '--name=<exe_name>.exe',
-        '--icon=src/images/icon.ico',
-        '--add-data=src/images/icon.png:images',
+        '--icon=src/<project>/images/icon.ico',
+        '--add-data=src/<project>/images/icon.png:images',
     ])
